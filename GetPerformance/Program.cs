23e3ba6a6b01ad68be1813a9214ddc2cb95a5028
hasitha.prageeth <haztha@gmail.com>
2015-05-08 19:35:29 +0530

    Client App Code 1.0
    Updated inputs

--- GetPerformance/Program.cs
+++ GetPerformance/Program.cs
@@ -9,88 +9,55 @@ using System.Speech.Synthesis;
 using System.Runtime.InteropServices;
 using System.Net.NetworkInformation;
 using System.Net;
+using System.Net.Sockets;
 
 namespace GetPerformance
 {
     class Program
     {
-       /* #region Hide Console
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        const int HIDE = 0;
-        #endregion*/
 
         private static SpeechSynthesizer speech = new SpeechSynthesizer();
-        
+         
         public static void Main(string[] args)
-        {
-           /* 
-            var handle = GetConsoleWindow();
-            // Hide
-            ShowWindow(handle, HIDE); */ 
-            
+        {    
             //Initialize the speed of the voice for the Warning !!!
             int speed = 2;
 
+            //Initialize the Mac Address 
+            string macAddresses = string.Empty;
+
             #region Start
             //Speak out the greeting to the user
-            speak("Welcome to your Workplace");
+            speak("Welcome to your Workplace", VoiceGender.Female);
             Thread.Sleep(2);
-            speak("Please enter your user name and password");
+            speak("Please enter your user name and password", VoiceGender.Female);
             #endregion
 
             #region Unique Identification
             //Get the current logged in user of the system
             Console.WriteLine(System.Security.Principal.WindowsIdentity.GetCurrent().Name);
 
-            //Get the Mac Address 
-            string macAddresses = string.Empty;
+            //Get the Mac Address                       
             foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
             {
                 if (nic.OperationalStatus == OperationalStatus.Up)
                 {
-                    macAddresses += nic.GetPhysicalAddress().ToString();
-                    break;
+                    macAddresses += string.Join(":", (from addr in nic.GetPhysicalAddress().GetAddressBytes() select addr.ToString("X2")).ToArray());                  
                 }
+ 
             }
             Console.WriteLine(macAddresses);
 
-            //Get the IP address of the computer
-             string name = Dns.GetHostName();
-             try
-             {
-                 IPAddress[] addrs = Dns.GetHostEntry(name).AddressList;
-                 foreach (IPAddress addr in addrs)
-                 Console.WriteLine("{0}/{1}", name, addr);
-             }
-             catch (Exception e)
-             {
-                 Console.WriteLine(e.Message);
-             }
-
-            string hostName = Dns.GetHostName(); // Retrive the Name of HOST
-            Console.WriteLine(hostName);
+            // Retrive the Name of HOST
+            string hostName = Dns.GetHostName();            
+            Console.WriteLine(hostName);  
+                           
            // Get the IP
-            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
-            Console.WriteLine("My IP Address is :"+myIP);
+            IPHostEntry host = Dns.GetHostEntry(hostName);
+            string myIP = string.Join(",", (from ip in host.AddressList where ip.AddressFamily == AddressFamily.InterNetwork select ip.ToString()).ToList());
+            Console.WriteLine("My IP Address is :" + myIP);
             Console.ReadKey();
-       
             
-           /* try
-            {
-                 IPHostEntry hostname = Dns.GetHostByName("localhost");
-                   IPAddress[] ip = hostname.AddressList;
-                   Console.WriteLine(ip[0].ToString());
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }*/
             #endregion
 
             #region Performance Counters
@@ -104,13 +71,51 @@ namespace GetPerformance
             memory.NextValue();
 
             //This will create performance counter object to get the total system up time and initialize it. 
-            PerformanceCounter systemuptime = new PerformanceCounter("System", "System Up Time");
-            systemuptime.NextValue();    
+            PerformanceCounter systemUpTime = new PerformanceCounter("System", "System Up Time");
+            systemUpTime.NextValue();
+
+            //This will create performance counter object to get the processes and initialize it. 
+            PerformanceCounter processes = new PerformanceCounter("System", "Processes");
+            processes.NextValue();
+
+            //This will create performance counter object to get the threads and initialize it. 
+            PerformanceCounter threads = new PerformanceCounter("System", "Threads");
+            threads.NextValue(); 
+
+            //This will create performance counter object to get the percentage disk time and initialize it. 
+            PerformanceCounter diskTime = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+            diskTime.NextValue();
+
+            //This will create performance counter object to get the percentage idle time and initialize it. 
+            PerformanceCounter idleTime = new PerformanceCounter("PhysicalDisk", "% Idle Time", "_Total");
+            idleTime.NextValue();
+
+            //This will create performance counter object to get the disk bytes/sec and initialize it. 
+            PerformanceCounter diskBytes = new PerformanceCounter("PhysicalDisk", "Disk Bytes/sec", "_Total");
+            diskBytes.NextValue();
+
+            //This will create performance counter object to get the disk transfers/sec and initialize it. 
+            PerformanceCounter diskTransfers = new PerformanceCounter("PhysicalDisk", "Disk Transfers/sec", "_Total");
+            diskTransfers.NextValue();
+
+            //This will create performance counter object to get the average disk sec/transfer and initialize it. 
+            PerformanceCounter avgDiskSecsTransfers = new PerformanceCounter("PhysicalDisk", "Avg. Disk sec/Transfer", "_Total");
+           avgDiskSecsTransfers.NextValue();
+
+            //This will create performance counter object to get the average disk bytes/transfer and initialize it. 
+           PerformanceCounter avgDiskBytesTransfers = new PerformanceCounter("PhysicalDisk", "Avg. Disk Bytes/Transfer", "_Total");
+            avgDiskBytesTransfers.NextValue();
+
+             //This will create performance counter object to get the average disk queue length and initialize it. 
+            PerformanceCounter avgDiskQueueLength = new PerformanceCounter("PhysicalDisk", "Avg. Disk Queue Length", "_Total");
+            avgDiskQueueLength.NextValue();
+   
+
             #endregion
 
             #region Program Logic 1
             //Create time span object
-            TimeSpan t = TimeSpan.FromSeconds(systemuptime.NextValue());
+            TimeSpan t = TimeSpan.FromSeconds(systemUpTime.NextValue());
             String uptime = String.Format("The current system up time is {0} days {1} hours {2} minutes and {3} seconds",
                     (int)t.TotalDays,
                     (int)t.Hours,
@@ -120,11 +125,12 @@ namespace GetPerformance
             
             Console.WriteLine(uptime);
             //Speak out current system up time
-            speak(uptime, VoiceGender.Male,2);
+            speak(uptime, VoiceGender.Female,2);
 
             // Get the Logical Disk size and free space
             selectQuery("Win32_LogicalDisk", "Size");
             selectQuery("Win32_LogicalDisk", "FreeSpace");
+            selectQuery("Win32_ComputerSystem", "TotalPhysicalMemory");
             
             #endregion
 
@@ -140,9 +146,32 @@ namespace GetPerformance
                 Console.WriteLine("CPU Load : " + finalCpuCounter + "%");
 
                 //Get the available memory in megabytes
-                int availablememory = (int)memory.NextValue();
+                int availableMemory = (int)memory.NextValue();
                 
-                Console.WriteLine("Available Memory : " + availablememory + " MB");
+                Console.WriteLine("Available Memory : " + availableMemory + " MB");
+
+                int noProcesses = (int)processes.NextValue();
+                int noThreads = (int)threads.NextValue();
+                int percentDiskTime = (int)diskTime.NextValue();
+                int percentIdleTime = (int)idleTime.NextValue();
+                int diskKiloBytesSec = (int)(diskBytes.NextValue() / 1024);
+                int diskTransfersSec = (int)diskTransfers.NextValue();
+                double diskFailureCheckAvgDiskSecsTransfers = Math.Round(avgDiskSecsTransfers.NextValue(), 3);
+                int diskEfficiencyCheckAvgDiskKiloBytesTransfer = (int)(avgDiskBytesTransfers.NextValue() / 1024);
+                double diskBottleneckCheckAvgDiskQueueLength = Math.Round(avgDiskQueueLength.NextValue(), 3); 
+
+
+
+                Console.WriteLine("Processes : " + noProcesses);
+                Console.WriteLine("Threads : " + noThreads);
+                Console.WriteLine("Disk Time: " + percentDiskTime + "%");
+                Console.WriteLine("Idle Time : " + percentIdleTime + "%");
+                Console.WriteLine("Throughput - Disk Kilo Bytes/Sec  : " + diskKiloBytesSec + " KB");
+                Console.WriteLine("Disk Utilization - Disk Transfers/Sec  : " + diskTransfersSec + " Transfers/s");
+                Console.WriteLine("Disk Failure Check - Avg Disk Secs/Transfers  : " + diskFailureCheckAvgDiskSecsTransfers + " sec/Transfer");
+                Console.WriteLine("Disk Efficiency Check - Avg Disk Kilo Bytes Transfer : " + diskEfficiencyCheckAvgDiskKiloBytesTransfer + " KB/Transfer");
+                Console.WriteLine("Disk Bottleneck Check - Avg Disk Queue Length : " + diskBottleneckCheckAvgDiskQueueLength + " requests");
+                Console.WriteLine("Available Memory : " + availableMemory + " MB");
 
                 //Warning generator to alert the user when processor usage is higher
                 if (finalCpuCounter > 85)
@@ -154,21 +183,21 @@ namespace GetPerformance
                             speed++;
                         }
                         String messagecpu = String.Format("Warning your CPU Load is 100 percent");
-                        speak(messagecpu, VoiceGender.Male, speed);
+                        speak(messagecpu, VoiceGender.Female, speed);
                     }
 
                     else
                     {
                         String messagecpu = String.Format("CPU Load {0} percent", finalCpuCounter);
-                        speak(messagecpu,VoiceGender.Male,2);
+                        speak(messagecpu,VoiceGender.Female,2);
                     }
                 }
 
                 //Warning generator to alert the user when available memory is lesser
-                if (availablememory < 200)
+                if (availableMemory < 200)
                 {
-                    String messagememory = String.Format("Available Memory {0} mega bytes", availablememory);
-                    speak(messagememory, VoiceGender.Male, 2);
+                    String messagememory = String.Format("Available Memory {0} mega bytes", availableMemory);
+                    speak(messagememory, VoiceGender.Female, 2);
                 }
                 Console.WriteLine();
             }
@@ -267,7 +296,7 @@ namespace GetPerformance
                      {
                          case "System.String[]":
                              string[] str = (string[])property.Value;
-                             string str1 = "";
+                             string str1 = string.Empty;
                              foreach (string s in str)
                              {
                                  str1 += s + " ";
@@ -278,7 +307,7 @@ namespace GetPerformance
 
                          case "System.UInt16[]":
                              ushort[] shortstr = (ushort[])property.Value;
-                             string shortstr1 = "";
+                             string shortstr1 = string.Empty;
                              foreach (ushort shrts in shortstr)
                              {
                                  shortstr1 += shrts.ToString() + " ";
@@ -305,14 +334,7 @@ namespace GetPerformance
 }
            
 
-/*server = "192.168.10.221";
-database = "restaurantdb";
-uid = "root";
-password = "";
-string connectionString;
-connectionString = "SERVER=" + server + "; PORT = 3306 ;" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-mycon = new MySqlConnection(connectionString);
-*/
+
 
 
 
